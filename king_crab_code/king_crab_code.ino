5fe6a6246d41020898b4cd9207524454b3817261
Sushi1725 <86946104+Sushi1725@users.noreply.github.com>
2025-03-22 17:05:09 +1100

    Create king_crab_code.ino
    Began the main code file. Made basic functions for the state machine in terms of calculating the netPoints inside the claws and assigned placeholder functions for what will occur dependant on netPoints (discard or keep the balls).
    
    Similarly, a function will be produced for when to tilt the camera to begin calculating the netPoints.

---
+++ king_crab_code/king_crab_code.ino
@@ -0,0 +1,90 @@
+/* Food Delivery Robot | King Crab | Team 8
+
+   This is the code for our food delivery robot.
+
+*/
+
+#include <Pixy2.h>
+#include <SPI.h>
+Pixy2 pixy; // Pixycam takes pins 10, 11, 12, and 13
+
+#include <Servo.h>
+Servo servo; // Servo on pin 9
+
+
+//The most important method in the Arduino library is getBlocks(), which returns the number of objects Pixy has detected. You can then look in the
+//pixy.ccc.blocks[] array for information about each detected object (one array member for each detected object.) Each array member (i) contains the following fields:
+//
+//pixy.ccc.blocks[i].m_signature The signature number of the detected object (1-7 for normal signatures)
+//pixy.ccc.blocks[i].m_x The x location of the center of the detected object (0 to 316)
+//pixy.ccc.blocks[i].m_y The y location of the center of the detected object (0 to 208)
+//pixy.ccc.blocks[i].m_width The width of the detected object (1 to 316)
+//pixy.ccc.blocks[i].m_height The height of the detected object (1 to 208)
+//pixy.ccc.blocks[i].m_angle The angle of the object detected object if the detected object is a color code (-180 to 180).
+//pixy.ccc.blocks[i].m_index The tracking index of the block
+//pixy.ccc.blocks[i].m_age The number of frames the block has been tracked.
+//pixy.ccc.blocks[i].print() A member function that prints the detected object information to the serial port
+
+// Function Prototypes
+int stateMachine_calculateBalls();
+int stateMachine();
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Starting...\n");
+
+  pixy.init();
+  servo.attach(9); //servo on pin 9
+}
+
+void loop() {
+  
+  int newVar = stateMachine_calculateBalls();
+  Serial.println(newVar);
+  
+
+}
+
+int stateMachine_calculateBalls() { // calculate the net points of balls in the claws
+  int netPoints = 0;
+
+  pixy.ccc.getBlocks();
+
+  if (pixy.ccc.numBlocks) { // if there are any balls in the FOV --> numBlocks is the amount of blocks    
+    for (int i = 0; i < pixy.ccc.numBlocks; i++) { // for each ball in FOV,
+      Serial.print("  block ");
+      Serial.print(i);
+      Serial.print(": ");
+      pixy.ccc.blocks[i].print();
+
+      if (pixy.ccc.blocks[i].m_signature == 1 || pixy.ccc.blocks[i].m_signature == 2) { // if signature (ball) is 1 or 2 (green/blue)
+        netPoints ++;
+      } else if (pixy.ccc.blocks[i].m_signature == 3) { // if signature is 3 (red)
+        netPoints += 2;
+      }
+    }
+  }
+  return netPoints;
+}
+
+int stateMachine() {
+  int netPoints = stateMachine_calculateBalls();
+  
+  if (netPoints < 0) {
+    //discard (to some other base)  
+  } else if (netPoints == 0) {
+    //dont care and discard  OR  continue grabbing balls
+  } else if (netPoints > 0) {
+    // go deposit  OR  continue grabbing balls
+  }
+}
+
+void tiltPixy() {
+  //get the (x,y) of balls onscreen
+  //if they get to the bottom of framee and leave, tilt
+
+  int pos = 0;
+  servo.write(pos);
+  delay(15);
+}
